e83498ac92f19ddd3720fdbafc99e71b5eaeb468
arifcanbaz <arifcanbaz908@gmail.com>
2023-11-19 23:14:11 +0300

    update

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const saat = document.getElementById("saat")
 const dakika = document.getElementById("dakika")
 const saniye = document.getElementById("saniye")
 const salise = document.getElementById("salise")
+const time = document.querySelector(".time")
 
 const pause = document.getElementById("pause")
 const play = document.getElementById("play")
 const reset = document.getElementById("reset")
 const playPause = document.getElementById("play-pause")
+const tur = document.getElementById("tur")
 
 //! kronometre süre akışı fonksiyonu
 
@@ -74,3 +76,81 @@ reset.addEventListener("click", () => {
     sayac = 0
 })
 
+// https://randomuser.me/api/  
+// https://restcountries.com/v3.1/all
+
+let times = []
+let sayac2 = 0
+
+tur.addEventListener("click", () => {
+    times.push(time.textContent)
+    dereceDomaYaz()
+    sayac2++
+})
+
+//! dereceyi doma yazdırma
+
+const dereceDomaYaz = () => {
+
+    let random = Math.floor(Math.random() * 250)
+    document.getElementById("derece-body").innerHTML += `
+    <tr>
+    <td>${sayac2+1}</td>
+    <td><img src="${ulke[random].flags.png}" width="35rem"></img></td>
+    <td>${athlete[sayac2].name.first} ${athlete[sayac2].name.last}</td>
+    <td>${times[sayac2]}</td>
+
+    </tr>
+    `
+}
+
+let ulke = []
+
+const getCountries = async () => {
+
+    try {
+    const res = await fetch(`https://restcountries.com/v3.1/all`)
+    if (!res.ok) {
+        throw new Error(`Something went wrong:${res.status}`)
+    }
+    const data = await res.json()
+    ulke = data
+
+    } catch (error) {
+        document.querySelector(".container1").innerHTML = `<h2>${error}</h2>`
+    }
+}
+
+window.addEventListener("load", () => {
+    getCountries()
+})
+
+let athlete = []
+
+const getUsers = async () => {
+    try {
+        const res = await fetch("https://randomuser.me/api/?gender=male")
+        if(!res.ok) {
+            throw new Error()
+        }
+        const data = await res.json()
+        show(data.results)
+    } catch (error) {
+        document.querySelector(".container1").innerHTML = `<h2>${error}</h2>`
+    }
+}
+
+window.addEventListener("load", () => {
+    getUsers()
+})
+
+console.log(athlete);
+
+const show = (results) => {
+
+    results.forEach((user) => {
+        athlete.push(user)
+    })
+
+    tur.addEventListener("click", getUsers)
+}
